07254f6ffe8a1d985896a4ad5eb03b16a9c607d1
Ilya <bonjurmrfirst@gmail.com>
2016-12-17 22:12:15 +0200

    added main nav, minor changes in sass file structure, changed eot fonts, IE8 now support cab*.eot

--- src/app/templates/header/header.directive.js
+++ src/app/templates/header/header.directive.js
@@ -2,7 +2,7 @@ angular.module('ahotelApp')
 
 	.directive('ahHeader', function() {
 		"use strict";
-		console.log(13);
+
 		return {
 			restrict: "EAC",
 			templateUrl: 'app/templates/header/header.html'
