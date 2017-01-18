de02bbf83e4e7996b8a73bc66fe844d1d6f90059
Ilya <bonjurmrfirst@gmail.com>
2017-01-18 20:22:18 +0200

    added resort filtrs service, minor changes

--- src/app/partials/resort/resort.controller.js
+++ src/app/partials/resort/resort.controller.js
@@ -5,22 +5,55 @@
         .module('ahotelApp')
         .controller('ResortController', ResortController);
 
-    ResortController.$inject = ['filtersService', 'resortService', '$scope'];
+    ResortController.$inject = ['filtersService', 'resortService', '$scope', 'hotelDetailsConstant'];
 
-    function ResortController(filtersService, resortService, $scope) {
+    function ResortController(filtersService, resortService, $scope, hotelDetailsConstant) {
+        /*this.obj = false;
         this.loading = true;
         this.hotels = {};
 
         this.filters = filtersService.initFilters();
 
-        resortService.getResort().then((response) => {
+        /!*resortService.getResort().then((response) => {
             this.hotels = response
-        });
+        });*!/
 
         $scope.$watch(() => this.filters, function(newValue) {//todo
             //for (let key in )
             console.log(newValue)
-        }, true);
+        }, true);*/
+
+        /*this.filters = {};
+
+        for (let key in hotelDetailsConstant) {
+            this.filters[key] = {};
+            for (let i = 0; i < hotelDetailsConstant[key].length; i++) {
+                this.filters[key][hotelDetailsConstant[key][i]] = true;
+            }
+        }
+
+        this.filters.price = {
+            min: 0,
+            max: 1000
+        };*/
+
+        this.loading = true;
+        this.hotels = {};
+
+        this.filters = filtersService.createFilters();
+
+        /*resortService.getResort().then((response) => {
+            this.hotels = filtersService.setModel(response).getModel();
+        });*/
+
+        /*$scope.$watch(() => this.filters,
+            (newValue) => {//todo
+                this.hotels = filtersService
+                    .applyFilter(newValue)
+                    .getModel();
+
+                console.log(this.hotels)
+            }, true);*/
 
         /*((response) => {
                 console.log(response)
@@ -29,5 +62,10 @@
             (response) => {
                 console.log(response)
             });*/
+
+        this.onFilterChange = function(filterGroup, filter, value) {
+            console.log(filterGroup, filter, value);
+            this.hotels = filtersService.applyFilter(filterGroup, filter, value).getModel();
+        }
     }
 })();
--- src/app/partials/resort/resort.filters.service.js
+++ src/app/partials/resort/resort.filters.service.js
@@ -8,7 +8,8 @@
     filtersService.$inject = ['hotelDetailsConstant', '$log'];
 
     function filtersService(hotelDetailsConstant, $log) {
-        let model,
+
+        /*let model,
             filteredModel,
             filters = {};
 
@@ -44,6 +45,149 @@
             initFilters: initFilters,
             setModel: setModel,
             applyFilters: applyFilters
+        };*/
+
+        function FiltersHandler(initFilters) {
+            this._initFilters = initFilters;
+            this._filters = {};
+
+            this._model = null;
+            this._filteredModel = null;
+        }
+
+        FiltersHandler.prototype.createFilters = function() {
+            let filters = {};
+
+            for (let key in this._initFilters) {
+                filters[key] = {};
+                for (let i = 0; i < this._initFilters[key].length; i++) {
+                    filters[key][this._initFilters[key][i]] = false;
+                }
+            }
+
+            filters.price = {
+                min: 0,
+                max: 1000
+            };
+
+            return filters
         };
+
+        FiltersHandler.prototype.applyFilter = function(newFilterGroup, newFilter, value) {
+            this._filteredModel = [];
+
+            this._filters = newFilters;
+            console.log(this._filters);
+
+            for (let hotel in this._model) {
+                let match = true;
+
+                for (let filterGroup in this._filters) {
+                    console.log(this._filters[filterGroup])
+                    for (let filter in this._filters[filterGroup]) {
+                        console.log(filter)
+                    }
+                }
+            }
+
+            if (value) {
+                this._filters[newFilterGroup] = this._filters[newFilterGroup] || {};
+                this._filters[newFilterGroup][newFilter] = true;
+            } else {
+                delete this._filters[newFilterGroup][newFilter];
+                if (Object.keys(this._filters[newFilterGroup]).length === 0) {
+                    delete this._filters[newFilterGroup]
+                }
+            }
+
+            if (Object.keys(this._filters).length === 0) {
+                this._filteredModel = this._model;
+
+                return this;
+            }
+
+            this._filteredModel = [];
+
+
+
+            return this;
+        };
+
+        FiltersHandler.prototype.getModel = function() {
+            return this._filteredModel;
+        };
+
+        FiltersHandler.prototype.setModel = function(model) {
+            this._model = model;
+            this._filteredModel = model;
+
+            return this;
+        };
+
+        return new FiltersHandler(hotelDetailsConstant)
     }
 })();
+/*
+
+ return {
+ initFilters: initFilters
+ };
+
+ function initFilters(obj) {
+ let filters = {};
+
+ for (let key in hotelDetailsConstant) {
+ filters[key] = {};
+ for (let i = 0; i < hotelDetailsConstant[key].length; i++) {
+ filters[key][hotelDetailsConstant[key][i]] = false;
+ }
+ }
+
+ filters.price = {
+ min: 0,
+ max: 1000
+ };
+
+ return filters
+ }
+
+ */
+
+
+/*let model,
+ filteredModel,
+ filters = {};
+
+ function initFilters() {
+ filters = {};
+
+ for (let key in hotelDetailsConstant) {
+ filters[key] = {};
+ for (let i = 0; i < hotelDetailsConstant[key].length; i++) {
+ filters[key][hotelDetailsConstant[key][i]] = false;
+ }
+ }
+
+ filters.price = {
+ min: 0,
+ max: 1000
+ };
+
+ return filters
+ }
+
+ function setModel(newModel) {
+ model = currenetModel;
+ }
+
+ function applyFilters(newFilters) {
+
+
+ return resultModel;
+ }
+
+ return {
+ initFilters: initFilters,
+ setModel: setModel,
+ applyFilters: applyFilters
+ };*/
